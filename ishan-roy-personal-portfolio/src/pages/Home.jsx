6b137005aadbc4b1ded163b06a7e10a05c4fa98e
Ishan Roy <iroy3@student.gsu.edu>
2025-09-25 14:47:06 -0400

    Set Background with Stars and Meteors

--- ishan-roy-personal-portfolio/src/pages/Home.jsx
+++ ishan-roy-personal-portfolio/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground"
 
 export const Home = () => {
     return (
@@ -7,7 +8,7 @@ export const Home = () => {
         {/*Theme Toggle*/}
         <ThemeToggle />
         {/*Background Effects*/}
-
+        <StarBackground />
         {/*Navbar*/}
 
         {/*Main Content*/}
