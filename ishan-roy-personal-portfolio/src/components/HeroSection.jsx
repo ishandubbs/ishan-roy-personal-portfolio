9e2efe27af9b2511b90c2e1a1be198f90e76d061
Ishan Roy <iroy3@student.gsu.edu>
2025-10-18 14:17:07 -0400

    Updated PFP and Images

--- ishan-roy-personal-portfolio/src/components/HeroSection.jsx
+++ ishan-roy-personal-portfolio/src/components/HeroSection.jsx
@@ -4,9 +4,9 @@ export const HeroSection = () => {
     return (
         <section id="hero" className="relative min-h-screen flex flex-col items-center justify-center px-4">
             <div className="container max-w-4xl mx-auto text-center z-10">
+                <img src="/pfp/ishan_roy_pfp_circle.png" alt="profile picture" className="mx-auto rounded-full w-48 h-48 object-cover"/>
                 <div className="space-y-6">
                     <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
-                        <span className="opacity-0 animate-fade-in">Hi I'm</span>
                         <span className="text-primary opacity-0 animate-fade-in-delay-1"> Ishan</span>
                         <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">Roy</span>
                     </h1>
