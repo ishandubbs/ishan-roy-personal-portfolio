08ba1fa4ee64c18a8c41f69e85ad3b93be6de0fe
Ishan Roy <ishandubbs@gmail.com>
2025-11-04 13:49:27 -0500

    Updated Ishan's Information

--- ishan-roy-personal-portfolio/src/components/AboutSection.jsx
+++ ishan-roy-personal-portfolio/src/components/AboutSection.jsx
@@ -10,59 +10,42 @@ export const AboutSection = () => {
 
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
                     <div className="space-y-6">
-                        <h3 className="text-2xl font-semibold">Luh Jit</h3>
-
-                        <p className="text-muted-foreground">A little bit about me</p>
+                        <p className="text-muted-foreground">Hello, my name is Ishan Roy, and I am a sophomore majoring in computer engineering at Georgia Tech. I have interests in web development, robotics, and software design, and I've built many projects that help turn ideas into impact. When I'm not coding, I enjoy hanging out with my family and friends, swimming, basketball, football, and playing video games!</p>
 
                         <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
                             <a href="#contact" className="cosmic-button">
                             Get in Touch
                             </a>
 
-                            <a href="#" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                            <a href="/Ishan_Roy___Resume.pdf" target="_blank" rel="nopener noreferrer" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                             Download Resume
                             </a>
                         </div>
                     </div>
 
-                    <div className="grid grid-cols-1 gap-6">
-                        <div className="gradient-border p-6 card-hover">
+                    <div className="grid grid-cols-1 gap-6 justify-center">
+                        <div className="gradient-border p-6 card-hover flex flex-col justify-center">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
                                     <Code className="h-6 w-6 text-primary"/>
                                 </div>
-                                <div className="text-left">
-                                    <h4 className="font-semibold text-lg">Something 1</h4>
-                                    <p className="text-muted-foreground">
-                                        Something 1
-                                    </p>
-                                </div>
+                                <h4 className="font-semibold text-lg">Distributed Systems & Software Design</h4>
                             </div>
                         </div>
-                        <div className="gradient-border p-6 card-hover">
+                        <div className="gradient-border p-6 card-hover flex flex-col justify-center">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
                                     <User className="h-6 w-6 text-primary"/>
                                 </div>
-                                <div className="text-left">
-                                    <h4 className="font-semibold text-lg">Something 2</h4>
-                                    <p className="text-muted-foreground">
-                                        Something 2
-                                    </p>
-                                </div>
+                                    <h4 className="font-semibold text-lg">Robotics</h4>
                             </div>
                         </div>
-                        <div className="gradient-border p-6 card-hover">
+                        <div className="gradient-border p-6 card-hover flex flex-col justify-center">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
                                     <Briefcase className="h-6 w-6 text-primary"/>
                                 </div>
-                                <div className="text-left">
-                                    <h4 className="font-semibold text-lg">Something 3</h4>
-                                    <p className="text-muted-foreground">
-                                        Something 3
-                                    </p>
-                                </div>
+                                    <h4 className="font-semibold text-lg">Web Development</h4>
                             </div>
                         </div>
                     </div>
--- ishan-roy-personal-portfolio/src/components/Experience/ExperienceSection.jsx
+++ ishan-roy-personal-portfolio/src/components/Experience/ExperienceSection.jsx
@@ -7,30 +7,21 @@ const experiences = [
         role: 'Academic Tutor',
         duration: 'January 2025 - Present',
         bullets: [
-            'Spent 5+ hours a week tutoring GSU students in math and computer science courses such as Differential Calculus and Object Oriented Programming, retaining knowledge in concepts like data structures and inheritance.',
-            'Maintained a 5.0/5.0 average rating across 10 student reviews, highlighting subject knowledge and teaching skills.'
-        ]
-    },
-
-    {
-        company: 'GSHacks',
-        location: 'Atlanta, GA',
-        role: 'Cofounder/Event Organizer',
-        duration: 'October 2024 - Present',
-        bullets: [
-            'Spent 5+ hours a week tutoring GSU students in math and computer science courses such as Differential Calculus and Object Oriented Programming, retaining knowledge in concepts like data structures and inheritance.',
-            'Maintained a 5.0/5.0 average rating across 10 student reviews, highlighting subject knowledge and teaching skills.'
+            'Provided one-on-one tutoring services in various subjects, including CS, Math, and Physics courses, focusing on key concepts and problem-solving strategies.',
+            "Assessed students' proficiency levels and help create tailored lesson plans and resources to strengthen their understanding and application on the topic.",
+            'Maintained a 4.9/5.0 average rating across 9 verified student reviews, highlighting patience and subject knowledge.'
         ]
     },
 
     {
         company: 'WhizLearning Kids',
         location: 'Lawrenceville, GA',
-        role: 'Student Assistant Intern',
+        role: 'Coding and Robotics Instructor',
         duration: 'June 2024 - May 2025',
         bullets: [
-            'Spent 5+ hours a week tutoring GSU students in math and computer science courses such as Differential Calculus and Object Oriented Programming, retaining knowledge in concepts like data structures and inheritance.',
-            'Maintained a 5.0/5.0 average rating across 10 student reviews, highlighting subject knowledge and teaching skills.'
+            'Collaborated with fellow instructors to develop a structured 10-week robotics curriculum, increasing student engagement by integrating real-world problem-solving challenges and teamwork activities.',
+            'Taught and mentored 20+ elementary and middle school students in coding and robotics, improving programming proficiency by creating weekly hands-on projects in Scratch, Arduino, and VexCode VR.',
+            'Designed 13 interactive Scratch games (including Geometry Dash, OnlyUp, and 3D Raycaster) to enhance understanding of programming logic and game design through project-based learning.'
         ]
     },
 
