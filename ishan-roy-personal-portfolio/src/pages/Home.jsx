fa955000c4d91f7f3b3ef39a6d4661d709ab5772
Ishan Roy <ishandubbs@gmail.com>
2025-11-06 13:35:46 -0500

    Update Home.jsx

--- ishan-roy-personal-portfolio/src/pages/Home.jsx
+++ ishan-roy-personal-portfolio/src/pages/Home.jsx
@@ -1,5 +1,13 @@
 import { Navbar } from "../components/Navbar";
 import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground"
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
+import { ProjectsSection } from "../components/ProjectsSection";
+import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
+import { ExperienceSection } from "../components/Experience/ExperienceSection";
 
 export const Home = () => {
     return (
