8afac0b26d60e3a69027901aa136ec54c6666347
Ishan Roy <ishandubbs@gmail.com>
2025-11-06 13:57:34 -0500

    Update vite.config.js

--- ishan-roy-personal-portfolio/vite.config.js
+++ ishan-roy-personal-portfolio/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',   // Output folder for production build
-  },
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 });
