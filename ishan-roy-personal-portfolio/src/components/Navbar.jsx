a6c4a971e7c930b570054aaf8c642d49dd20585c
Ishan Roy <ishandubbs@gmail.com>
2025-11-06 13:23:03 -0500

    changed @ to ..

--- ishan-roy-personal-portfolio/src/components/Navbar.jsx
+++ ishan-roy-personal-portfolio/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils";
+import { cn } from "../lib/utils";
 import { useEffect, useState } from "react";
 import { X } from "lucide-react";
 import { Menu } from "lucide-react";
